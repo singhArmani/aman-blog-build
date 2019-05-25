4295200e46365c0d6c9283ac0dd634cbf9a638ad
Amandeep <amandeep.singh@4mation.com>
2019-05-25 21:10:58 +1000

    hidden gems of debugging

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1bc1c81906e4eac7f20c.js"
+    "url": "webpack-runtime-63fac1ad8099bdd9c25d.js"
   },
   {
     "url": "styles.64665bfcddb3d13ea433.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c029e1e7120c20feaba1a398216eca79"
+    "revision": "6acdda3bae7e9e449ec7e0542fb013df"
   },
   {
     "url": "component---src-pages-404-js-43c4d451065039a074e5.js"
